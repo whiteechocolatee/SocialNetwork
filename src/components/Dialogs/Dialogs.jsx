97fad30577f1e6f6a5c68849f74f0145a77b8294
whiiteechocolatee <creatuseandr@icloud.com>
2020-06-22 15:58:40 +0300

    add same arry.map into MyPost

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -25,11 +25,8 @@ let dialogsData = [
   { name: "Kseniya", id: 4 },
   { name: "Denis", id: 5 },
 ];
-let dialogsElements = dialogsData.map((dialog) => (
-  <DialogItem name={dialog.name} id={dialog.id} />
-));
 
-let MessageData = [
+let messageData = [
   { message: "Hi!", id: 1 },
   { message: "How are u?", id: 2 },
   { message: "Fine!", id: 3 },
@@ -37,7 +34,11 @@ let MessageData = [
   { message: "What u say about...", id: 5 },
 ];
 
-let messageElements = MessageData.map((message) => (
+let dialogsElements = dialogsData.map((dialog) => (
+  <DialogItem name={dialog.name} id={dialog.id} />
+));
+
+let messageElements = messageData.map((message) => (
   <Message message={message.message} id={message.id} />
 ));
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-// import "./Profile.module.css";
 import c from "./MyPosts.module.css";
 import Post from "./Post/Post";
-// import Likes from "./Post/Likes";
 
 const MyPosts = () => {
   let postData = [
-    { post: "Hi man", id: 1, likesCount: 42 },
-    { post: "i will be fine!", id: 2, likesCount: 23 },
+    { post: "Hi man!!", id: 1, likesCount: 42 },
+    { post: "i am fine!", id: 2, likesCount: 23 },
   ];
+
+  let postElements = postData.map((post) => (
+    <Post message={post.post} quantity={post.likesCount} />
+  ));
 
   return (
     <div className={c.content}>
@@ -25,11 +27,7 @@ const MyPosts = () => {
         </div>
       </div>
       <div>new post</div>
-      <div className={c.posts}>
-        <Post message={postData[0].post} quantity={postData[0].likesCount} />
-
-        <Post message={postData[1].post} quantity={postData[1].likesCount} />
-      </div>
+      <div className={c.posts}>{postElements}</div>
     </div>
   );
 };

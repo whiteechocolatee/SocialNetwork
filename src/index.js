ef1a4e7232fce993746e52c72e32606500d10595
whiiteechocolatee <creatuseandr@icloud.com>
2020-07-08 22:04:06 +0300

    edit component by store method

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import state, { subscribe } from "./stateData/state";
+// import state, { subscribe } from "./stateData/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { addPost } from "./stateData/state";
-import { updateNewPost} from "./stateData/state";
+import store from "./stateData/state";
+// import { addPost } from "./stateData/state";
+// import { updateNewPost } from "./stateData/state";
 import { BrowserRouter } from "react-router-dom";
 
-
-let rerenderTree = (state) => {
+let rerenderTree = () => {
   ReactDOM.render(
     <BrowserRouter>
       <React.StrictMode>
-        <App state={state} 
-       updateNewPost={updateNewPost}
-        addPost={addPost} />
+        <App
+          state={store.getState()}
+          updateNewPost={store.updateNewPost.bind(store)}
+          addPost={store.addPost.bind(store)}
+        />
       </React.StrictMode>
     </BrowserRouter>,
     document.getElementById("root")
   );
 };
 
-rerenderTree(state);
+rerenderTree();
 
-subscribe(rerenderTree);
+store.subscribe(rerenderTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

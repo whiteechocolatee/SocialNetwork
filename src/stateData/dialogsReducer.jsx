43227dae3465b5beef1933436217c7d7b43cda48
whiiteechocolatee <creatuseandr@icloud.com>
2020-07-28 22:20:31 +0300

    create reducer function for dialogs

--- src/stateData/dialogsReducer.jsx
+++ src/stateData/dialogsReducer.jsx
@@ -1,7 +1,19 @@
-const dialogsReducer = (state,action) =>{
+const ADD_MESSAGE = "ADD-MESSAGE";
+const UPDATE_NEW_MESSAGE = "UPDATE-NEW-MESSAGE";
 
+const dialogsReducer = (state, action) => {
+  if (action.type === ADD_MESSAGE) {
+    let myMessage = {
+      message: state.newMessageText,
+      id: 6,
+    };
+    state.messages.push(myMessage);
+    state.newMessageText = "";
+  } else if (action.type === UPDATE_NEW_MESSAGE) {
+    state.newMessageText = action.newMessage;
+  }
 
-    return state;
-}
+  return state;
+};
 
 export default dialogsReducer;

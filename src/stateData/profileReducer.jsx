800afd4592baba14c816beddc1971b04ca43e730
whiiteechocolatee <creatuseandr@icloud.com>
2020-07-30 16:29:24 +0300

    refactoring

--- src/stateData/profileReducer.jsx
+++ src/stateData/profileReducer.jsx
@@ -1,7 +1,7 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST = "UPDATE-NEW-POST";
 
-const profileReducer = (action, state) => {
+const profileReducer = (state, action) => {
   if (action.type === ADD_POST) {
     let myPost = {
       message: state.newPostText,

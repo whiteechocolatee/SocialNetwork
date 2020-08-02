90d475bdc43993bbee4bbaa8c441763f90b012c6
whiiteechocolatee <creatuseandr@icloud.com>
2020-08-02 19:49:21 +0300

    create redux func in redux store

---
+++ src/stateData/redux-store.js
@@ -0,0 +1,14 @@
+import profileReducer from "./profileReducer";
+import dialogsReducer from "./dialogsReducer";
+const { createStore, combineReducers } = require("redux");
+
+
+
+let reducersCombine = combineReducers({
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+});
+
+let store = createStore(reducersCombine);
+
+export default store;

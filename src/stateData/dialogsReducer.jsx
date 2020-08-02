db3c0eba866930fc30f54c6e11a7ff849ff17de6
whiiteechocolatee <creatuseandr@icloud.com>
2020-08-02 19:53:03 +0300

    create initState for the my dialogsPage

--- src/stateData/dialogsReducer.jsx
+++ src/stateData/dialogsReducer.jsx
@@ -1,7 +1,26 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE = "UPDATE-NEW-MESSAGE";
 
-const dialogsReducer = (state, action) => {
+let initState = {
+  messages: [
+    { message: "Hi!", id: 1 },
+    { message: "How are u?", id: 2 },
+    { message: "Fine!", id: 3 },
+    { message: "Hello guys!", id: 4 },
+    { message: "What u say about...", id: 5 },
+  ],
+
+  dialogs: [
+    { name: "Liza", id: 1 },
+    { name: "Nikita", id: 2 },
+    { name: "Kirill", id: 3 },
+    { name: "Kseniya", id: 4 },
+    { name: "Denis", id: 5 },
+  ],
+  newMessageText: "...",
+};
+
+const dialogsReducer = (state = initState, action) => {
   if (action.type === ADD_MESSAGE) {
     let myMessage = {
       message: state.newMessageText,

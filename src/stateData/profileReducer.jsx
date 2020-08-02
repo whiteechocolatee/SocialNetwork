4eddb565604cd0d1d5e4d7f0221285d6f1b6ee0f
whiiteechocolatee <creatuseandr@icloud.com>
2020-08-02 19:50:41 +0300

    create initial state for the prfile page/reducer

--- src/stateData/profileReducer.jsx
+++ src/stateData/profileReducer.jsx
@@ -1,7 +1,18 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST = "UPDATE-NEW-POST";
 
-const profileReducer = (state, action) => {
+let initState = {
+  posts: [
+    { message: "Hi man!!", id: 1, likesCount: 42 },
+    { message: "i am fine!", id: 2, likesCount: 23 },
+    { message: "The greatest page ever!!!!", id: 3, likesCount: 12 },
+    { message: "blablabla", id: 4, likesCount: 63 },
+    { message: "haha", id: 5, likesCount: 17 },
+  ],
+  newPostText: "...",
+};
+
+const profileReducer = (state = initState, action) => {
   if (action.type === ADD_POST) {
     let myPost = {
       message: state.newPostText,

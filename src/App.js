146f89aac81397675f9d598c80b2cc544353d1d0
whiiteechocolatee <creatuseandr@icloud.com>
2020-08-11 22:36:01 +0300

    del some components from import

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import { Route } from "react-router-dom";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
   return (
@@ -27,7 +27,7 @@ const App = (props) => {
         <Route
           path="/Dialogs"
           render={() => (
-            <Dialogs
+            <DialogsContainer
               state={props.state.dialogsPage}
               dispatch={props.dispatch}
               store={props.store}

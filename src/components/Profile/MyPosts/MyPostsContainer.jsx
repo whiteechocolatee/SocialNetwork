50fc7264cb59b90edec9eeaaaf21d91926fd33b8
whiiteechocolatee <creatuseandr@icloud.com>
2020-08-16 18:32:13 +0300

    can add comment to profile

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -7,25 +7,26 @@ import MyPosts from "./MyPosts";
 import StoreContext from "../../../StoreContext";
 
 const MyPostsContainer = (props) => {
-  let addPost = () => {
-    props.dispatch(addPostActionCreator());
-  };
-
-  let onPostChange = (text) => {
-    let action = onPostChangeActionCreator(text);
-    props.dispatch(action);
-  };
-
   return (
     <StoreContext>
-      {(store) => (
-        <MyPosts
-          newPostText={store.getState().profilePage.newPostText}
-          updateNewPostText={onPostChange}
-          addPost={addPost}
-          posts={store.getState().profilePage.posts}
-        />
-      )}
+      {(store) => {
+        let addPost = () => {
+          props.dispatch(addPostActionCreator());
+        };
+
+        let onPostChange = (text) => {
+          let action = onPostChangeActionCreator(text);
+          props.dispatch(action);
+        };
+        return (
+          <MyPosts
+            newPostText={store.getState().profilePage.newPostText}
+            updateNewPostText={onPostChange}
+            addPost={addPost}
+            posts={store.getState().profilePage.posts}
+          />
+        );
+      }}
     </StoreContext>
   );
 };

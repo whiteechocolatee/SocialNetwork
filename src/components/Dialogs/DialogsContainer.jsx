8357987623cff32d112d171c7ab580978f68b06c
whiiteechocolatee <creatuseandr@icloud.com>
2020-08-17 15:15:21 +0300

    create new ways for func to dialogs

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -4,27 +4,34 @@ import {
   addMessageCreator,
 } from "../../stateData/dialogsReducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext";
 
 const DialogsContainer = (props) => {
-  let addMessage = () => {
-    props.dispatch(addMessageCreator());
-  };
-
-  let onMessageChange = (text) => {
-    let action = onMessageChangeCreator(text);
-    props.dispatch(action);
-  };
-
+  debugger;
   return (
-    <Dialogs
-      addMessage={addMessage}
-      updateNewMessage={onMessageChange}
-      state={props.state}
-      dispatch={props.dispatch}
-      store={props.store}
-      dialogs={props.state.dialogs}
-      messages={props.state.messages}
-    />
+    <StoreContext.Consumer>
+      {(store) => {
+        let addMessage = () => {
+          props.dispatch(addMessageCreator());
+        };
+
+        let onMessageChange = (text) => {
+          let action = onMessageChangeCreator(text);
+          props.dispatch(action);
+        };
+        return (
+          <Dialogs
+            addMessage={addMessage}
+            updateNewMessage={onMessageChange}
+            state={store.getState().state}
+            dispatch={store.getState().dispatch}
+            store={store.getState().store}
+            dialogs={store.getState().dialogsPage.dialogs}
+            messages={store.getState().dialogsPage.messages}
+          />
+        );
+      }}
+    </StoreContext.Consumer>
   );
 };
 

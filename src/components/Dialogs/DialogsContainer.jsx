1a638821136af05b5ae13b1ce18c5b0fa7d2681f
whiiteechocolatee <creatuseandr@icloud.com>
2020-09-02 20:31:20 +0300

    some changes

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -6,33 +6,6 @@ import {
 import Dialogs from "./Dialogs";
 import { connect } from "react-redux";
 
-// const DialogsContainer = (props) => {
-//   return (
-//     <StoreContext.Consumer>
-//       {(store) => {
-//         let addMessage = () => {
-//           props.dispatch(addMessageCreator());
-//         };
-
-//         let onMessageChange = (text) => {
-//           let action = onMessageChangeCreator(text);
-//           props.dispatch(action);
-//         };
-//         return (
-//           <Dialogs
-//             addMessage={addMessage}
-//             updateNewMessage={onMessageChange}
-//             dialogs={store.getState().dialogsPage.dialogs}
-//             messages={store.getState().dialogsPage.messages}
-//             newMessageText={store.getState().dialogsPage.newMessageText}
-//           />
-//         );
-//       }}
-//     </StoreContext.Consumer>
-//   );
-// };
-// export default DialogsContainer;
-
 const mapStateToProps = (store) => {
   debugger;
   return {

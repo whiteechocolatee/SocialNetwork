d5b9b7c34a5da74f63126842119e3bc9ade2a72e
whiiteechocolatee <creatuseandr@icloud.com>
2020-09-03 22:18:00 +0300

    create users three in the redux store

--- src/stateData/redux-store.js
+++ src/stateData/redux-store.js
@@ -1,10 +1,12 @@
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
+import usersReducer from "./usersReducer";
 const { createStore, combineReducers } = require("redux");
 
 let reducersCombine = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
+  usersPage: usersReducer,
 });
 
 let store = createStore(reducersCombine);

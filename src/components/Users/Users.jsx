070e81d1b0b867199758a9d2fdd101e79a86a135
whiiteechocolatee <creatuseandr@icloud.com>
2020-09-04 22:01:48 +0300

    create some UI and modified buttons

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,50 @@
-import react from "react";
+import React from "react";
 
-const Users = () => {
-  return;
+let Users = (props) => {
+  return (
+    <div>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img src={u.photoUrl} />
+            </div>
+            <div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                )}
+              </div>
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.fullName}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{u.location.country}</div>
+              <div>{u.location.city}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
